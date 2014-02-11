3ef747eca9e3a22507e196aa41cb03fa11173477
Kevin Ko <kevin.s.ko@gmail.com>
2014-02-11 10:39:33 -0600

    Add http error code to constructor.

--- src/main/com/faveset/khttp/InvalidRequestException.java
+++ src/main/com/faveset/khttp/InvalidRequestException.java
@@ -3,7 +3,21 @@
 package com.faveset.khttp;
 
 class InvalidRequestException extends Exception {
+    private int mErrorCode;
+
     public InvalidRequestException(String reason) {
         super(reason);
+    }
+
+    public InvalidRequestException(String reason, int errorCode) {
+        super(reason);
+        mErrorCode = errorCode;
+    }
+
+    /**
+     * @return The error code or 0 if none.
+     */
+    public int getErrorCode() {
+        return mErrorCode;
     }
 }
