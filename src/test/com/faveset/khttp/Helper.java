d7e93cf23893519c9d3d7564d4fd5bf98c3a475d
Kevin Ko <kevin.s.ko@gmail.com>
2014-02-11 15:19:12 -0600

    Factor out test helper code to Helper.

---
+++ src/test/com/faveset/khttp/Helper.java
@@ -0,0 +1,21 @@
+// Copyright 2014, Kevin Ko <kevin@faveset.com>
+
+package com.faveset.khttp;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+
+import static org.junit.Assert.assertTrue;
+
+class Helper {
+    public static final Charset US_ASCII_CHARSET = Charset.forName("US-ASCII");
+
+    public static void compare(ByteBuffer buf, String v) {
+        String dataStr = new String(buf.array(), buf.position(), buf.remaining(), US_ASCII_CHARSET);
+        assertTrue(dataStr.equals(v));
+    }
+
+    public static ByteBuffer makeByteBuffer(String s) {
+        return ByteBuffer.wrap(s.getBytes(US_ASCII_CHARSET));
+    }
+}
--- src/test/com/faveset/khttp/StringsTest.java
+++ src/test/com/faveset/khttp/StringsTest.java
@@ -21,26 +21,17 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class StringsTest {
-    private static final Charset US_ASCII_CHARSET = Charset.forName("US-ASCII");
-
-    private void compare(ByteBuffer buf, String v) {
-        String dataStr = new String(buf.array(), buf.position(), buf.remaining(), US_ASCII_CHARSET);
-        assertTrue(dataStr.equals(v));
-    }
-
-    private ByteBuffer makeByteBuffer(String s) {
-        return ByteBuffer.wrap(s.getBytes(US_ASCII_CHARSET));
-    }
+    private Helper mHelper = new Helper();
 
     @Test
     public void testHasCrlf() {
-        assertTrue(Strings.hasLeadingCrlf(makeByteBuffer("\n")));
-        assertTrue(Strings.hasLeadingCrlf(makeByteBuffer("\r\n")));
-        assertFalse(Strings.hasLeadingCrlf(makeByteBuffer(" \n")));
-        assertFalse(Strings.hasLeadingCrlf(makeByteBuffer(" n")));
-        assertFalse(Strings.hasLeadingCrlf(makeByteBuffer(" \r\n")));
+        assertTrue(Strings.hasLeadingCrlf(Helper.makeByteBuffer("\n")));
+        assertTrue(Strings.hasLeadingCrlf(Helper.makeByteBuffer("\r\n")));
+        assertFalse(Strings.hasLeadingCrlf(Helper.makeByteBuffer(" \n")));
+        assertFalse(Strings.hasLeadingCrlf(Helper.makeByteBuffer(" n")));
+        assertFalse(Strings.hasLeadingCrlf(Helper.makeByteBuffer(" \r\n")));
 
-        ByteBuffer buf = makeByteBuffer(" \r\n");
+        ByteBuffer buf = Helper.makeByteBuffer(" \r\n");
         int oldPos = buf.position();
         assertFalse(Strings.hasLeadingCrlf(buf));
         assertEquals(oldPos, buf.position());
@@ -49,44 +40,44 @@ public class StringsTest {
     @Test
     public void testParseLine() {
         String testStr = "line1\nline2\r\n\nline3";
-        ByteBuffer buf = ByteBuffer.wrap(testStr.getBytes(US_ASCII_CHARSET));
+        ByteBuffer buf = ByteBuffer.wrap(testStr.getBytes(Helper.US_ASCII_CHARSET));
 
         ByteBuffer result = Strings.parseLine(buf);
-        compare(result, "line1");
+        Helper.compare(result, "line1");
 
         result = Strings.parseLine(buf);
-        compare(result, "line2\r");
+        Helper.compare(result, "line2\r");
 
         result = Strings.parseLine(buf);
-        compare(result, "");
+        Helper.compare(result, "");
 
         result = Strings.parseLine(buf);
         assertEquals(result, null);
         ByteBuffer cmp = buf.duplicate();
         cmp.flip();
-        compare(cmp, "line3");
+        Helper.compare(cmp, "line3");
 
         // Now, try appending.
         String appendStr = "line3b\nline4";
-        ByteBuffer appendBuf = ByteBuffer.wrap(appendStr.getBytes(US_ASCII_CHARSET));
+        ByteBuffer appendBuf = ByteBuffer.wrap(appendStr.getBytes(Helper.US_ASCII_CHARSET));
         buf.put(appendBuf);
         buf.flip();
 
         // Compare the appended buffer.
         result = Strings.parseLine(buf);
-        compare(result, "line3line3b");
+        Helper.compare(result, "line3line3b");
 
         result = Strings.parseLine(buf);
         assertEquals(result, null);
         cmp = buf.duplicate();
         cmp.flip();
-        compare(cmp, "line4");
+        Helper.compare(cmp, "line4");
     }
 
     @Test(expected=BufferOverflowException.class)
     public void testParseLineOverflow() {
         String testStr = "line1";
-        ByteBuffer buf = ByteBuffer.wrap(testStr.getBytes(US_ASCII_CHARSET));
+        ByteBuffer buf = ByteBuffer.wrap(testStr.getBytes(Helper.US_ASCII_CHARSET));
         // This should throw an exception, since we assume that buf is of fixed
         // size and nothing can be appended to it to lead to a newline.
         Strings.parseLine(buf);
@@ -95,58 +86,58 @@ public class StringsTest {
     @Test
     public void testParseWord() {
         String testStr = "\n\r \tword1 word2  word3";
-        ByteBuffer buf = ByteBuffer.wrap(testStr.getBytes(US_ASCII_CHARSET));
+        ByteBuffer buf = ByteBuffer.wrap(testStr.getBytes(Helper.US_ASCII_CHARSET));
         assertEquals(Strings.parseWord(buf), "word1");
         assertEquals(Strings.parseWord(buf), "word2");
         assertEquals(Strings.parseWord(buf), "word3");
         assertEquals(Strings.parseWord(buf), "");
 
         testStr = "word1\n";
-        buf = ByteBuffer.wrap(testStr.getBytes(US_ASCII_CHARSET));
+        buf = ByteBuffer.wrap(testStr.getBytes(Helper.US_ASCII_CHARSET));
         assertEquals(Strings.parseWord(buf), "word1");
         assertEquals(Strings.parseWord(buf), "");
     }
 
     @Test
     public void testParseVersion() throws ParseException {
-        ByteBuffer buf = makeByteBuffer("HTTP/1.0");
+        ByteBuffer buf = Helper.makeByteBuffer("HTTP/1.0");
         assertEquals(Strings.parseHttpVersion(buf), 0);
 
-        buf = makeByteBuffer("HTTP/1.1");
+        buf = Helper.makeByteBuffer("HTTP/1.1");
         assertEquals(Strings.parseHttpVersion(buf), 1);
 
-        buf = makeByteBuffer("HTTP/1.12");
+        buf = Helper.makeByteBuffer("HTTP/1.12");
         assertEquals(Strings.parseHttpVersion(buf), 12);
     }
 
     @Test(expected=ParseException.class)
     public void testParseVersionException() throws ParseException {
-        ByteBuffer buf = makeByteBuffer("HTTP /1.2");
+        ByteBuffer buf = Helper.makeByteBuffer("HTTP /1.2");
         Strings.parseHttpVersion(buf);
     }
 
     @Test
     public void testParseText() {
-        ByteBuffer buf = makeByteBuffer("Hello: world");
+        ByteBuffer buf = Helper.makeByteBuffer("Hello: world");
         assertEquals(Strings.parseText(buf), "Hello: world");
         assertFalse(buf.hasRemaining());
 
-        buf = makeByteBuffer("Hello: world\037");
+        buf = Helper.makeByteBuffer("Hello: world\037");
         assertEquals(Strings.parseText(buf), "Hello: world");
         assertEquals(buf.get(), '\037');
     }
 
     @Test
     public void testParseToken() {
-        ByteBuffer buf = makeByteBuffer("Hello: world");
+        ByteBuffer buf = Helper.makeByteBuffer("Hello: world");
         assertEquals(Strings.parseToken(buf), "Hello");
         assertEquals(buf.get(), ':');
 
-        buf = makeByteBuffer(":");
+        buf = Helper.makeByteBuffer(":");
         assertEquals(Strings.parseToken(buf), "");
         assertEquals(buf.get(), ':');
 
-        buf = makeByteBuffer("");
+        buf = Helper.makeByteBuffer("");
         assertEquals(Strings.parseToken(buf), "");
         assertFalse(buf.hasRemaining());
     }
