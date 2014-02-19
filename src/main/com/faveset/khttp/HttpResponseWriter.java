260403b42a72e640abc3641c7a7d7c7d5bc71bea
Kevin Ko <kevin.s.ko@gmail.com>
2014-02-19 14:52:44 -0600

    Add HttpResponseWriter interface.

--- src/main/com/faveset/khttp/HttpResponseWriter.java
+++ src/main/com/faveset/khttp/HttpResponseWriter.java
@@ -2,5 +2,33 @@
 
 package com.faveset.khttp;
 
-class HttpResponseWriter {
+import java.nio.ByteBuffer;
+
+import java.util.Map;
+
+public interface HttpResponseWriter {
+    HeadersBuilder getHeadersBuilder();
+
+    /**
+     * Add buf to the response body.
+     *
+     * This will implicitly call writeHeader with status OK if not already
+     * performed by the caller.
+     */
+    void write(ByteBuffer buf);
+
+    /**
+     * Write s to the response body.
+     *
+     * This will implicitly call writeHeader with status OK if not already
+     * performed by the caller.
+     */
+    void write(String s);
+
+    /**
+     * Prepares and writes an HTTP response header with given status code.
+     * If not called, the other write methods will call this implicitly with
+     * status OK.
+     */
+    void writeHeader(int statusCode);
 }
