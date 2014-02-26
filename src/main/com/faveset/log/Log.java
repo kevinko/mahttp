0b7400fb6fd4d90eefe49da30ffb8049e3b18f83
Kevin Ko <kevin.s.ko@gmail.com>
2014-02-26 16:00:12 -0600

    Clarify return value.

--- src/main/com/faveset/log/Log.java
+++ src/main/com/faveset/log/Log.java
@@ -5,51 +5,71 @@ package com.faveset.log;
 public interface Log {
     /**
      * Send a DEBUG log message.
+     *
+     * @return the number of bytes written
      */
     int d(String tag, String msg);
 
     /**
      * Send a DEBUG log message and log the exception.
+     *
+     * @return the number of bytes written
      */
     int d(String tag, String msg, Throwable tr);
 
     /**
      * Send an ERROR log message.
+     *
+     * @return the number of bytes written
      */
     int e(String tag, String msg);
 
     /**
      * Send an ERROR log message and log the exception.
+     *
+     * @return the number of bytes written
      */
     int e(String tag, String msg, Throwable tr);
 
     /**
      * Send an INFO log message.
+     *
+     * @return the number of bytes written
      */
     int i(String tag, String msg);
 
     /**
      * Send an INFO log message and log the exception.
+     *
+     * @return the number of bytes written
      */
     int i(String tag, String msg, Throwable tr);
 
     /**
      * Send a VERBOSE log message.
+     *
+     * @return the number of bytes written
      */
     int v(String tag, String msg);
 
     /**
      * Send a VERBOSE log message and log the exception.
+     *
+     * @return the number of bytes written
      */
     int v(String tag, String msg, Throwable tr);
 
     /**
      * Send a WARN log message.
+     *
+     * @return the number of bytes written
      */
     int w(String tag, String msg);
 
     /**
      * Send a WARN log message and log the exception.
+     *
+     * @return the number of bytes written
      */
     int w(String tag, String msg, Throwable tr);
 }
