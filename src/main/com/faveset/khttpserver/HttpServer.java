c6a26e8b364defdce17a9cda2ef054af3a8c9c80
Kevin Ko <kevin.s.ko@gmail.com>
2014-03-11 11:23:52 -0500

    Add simple handler for demonstrating usage.

--- src/main/com/faveset/khttpserver/HttpServer.java
+++ src/main/com/faveset/khttpserver/HttpServer.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 import sun.misc.Signal;
 import sun.misc.SignalHandler;
 
+import com.faveset.khttp.HttpHandler;
+import com.faveset.khttp.HttpRequest;
+import com.faveset.khttp.HttpResponseWriter;
+
 import com.faveset.log.OutputStreamLog;
 
 public class HttpServer {
@@ -22,11 +26,20 @@ public class HttpServer {
         final com.faveset.khttp.HttpServer server = new com.faveset.khttp.HttpServer();
         server.setLog(log);
 
-        // Set up the signal handler.
+        // Set up the signal handler for exiting the server.
         Signal.handle(new Signal("INT"), new SignalHandler() {
             @Override
             public void handle(Signal sig) {
                 server.stop();
+            }
+        });
+
+        // Register handlers.
+
+        server.registerHandler("/", new HttpHandler() {
+            public void onRequest(HttpRequest req, HttpResponseWriter w) {
+                w.getHeadersBuilder().set("Content-Type", "text/html");
+                w.write("<html><body>Hello</body></html>");
             }
         });
         server.listenAndServe("::", port);
