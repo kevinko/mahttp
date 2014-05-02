7b8ce05c57f4066a8dcec3a2c3802494dc0db1e5
Kevin Ko <kevin.s.ko@gmail.com>
2014-05-02 11:04:03 -0500

    Add SSLState interface.

---
+++ src/main/com/faveset/khttp/SSLState.java
@@ -0,0 +1,25 @@
+// Copyright 2014, Kevin Ko <kevin@faveset.com>
+
+package com.faveset.khttp;
+
+import java.nio.ByteBuffer;
+
+interface SSLState {
+    enum OpResult {
+        NONE,
+        DRAIN_DEST_BUFFER,
+        ENGINE_CLOSE,
+        LOAD_SRC_BUFFER,
+        STATE_CHANGE,
+    }
+
+    /**
+     * Unwraps from src to dest.
+     */
+    OpResult stepUnwrap(NetReader src, NetBuffer dest);
+
+    /**
+     * Wraps from src to dest.
+     */
+    OpResult stepWrap(NetReader src, NetBuffer dest);
+}
