e8e235cb1d900bd261ade7acd4a075f309dc91ba
Kevin Ko <kevin.s.ko@gmail.com>
2014-05-02 22:45:15 -0500

    Add initial (reviewed) SSLHandshakeState.

---
+++ src/main/com/faveset/khttp/SSLHandshakeState.java
@@ -0,0 +1,104 @@
+// Copyright 2014, Kevin Ko <kevin@faveset.com>
+
+package com.faveset.khttp;
+
+import javax.net.ssl.SSLEngine;
+
+class SSLHandshakeState {
+    private SSLEngine mSSLEngine;
+
+    public SSLHandshakeState(SSLEngine engine) {
+        mSSLEngine = engine;
+    }
+
+    @Override
+    public OpResult stepUnwrap(NetReader src, NetBuffer dest) {
+        do {
+            SSLEngineResult result = src.unwrap(mSSLEngine, dest);
+            switch (result.getStatus()) {
+                case BUFFER_OVERFLOW:
+                    // When handshaking, no application data is unwrapped.
+                    // Thus, this should never happen.  Instead, this would indicate some sort
+                    // of change in state.  Break out to the subsequent switch to handle things.
+                    break;
+
+                case BUFFER_UNDERFLOW:
+                    // We need more data from the network.
+                    return OpResult.LOAD_SRC_BUFFER;
+
+                case CLOSED:
+                    return OpResult.ENGINE_CLOSE;
+
+                case OK:
+                    src.updateRead();
+
+                    // Be greedy, and continue unwrapping.
+                    break;
+            }
+
+            switch (result.getHandshakeStatus()) {
+                case NEED_TASK:
+                    return OpResult.SCHEDULE_TASKS;
+
+                case NEED_UNWRAP:
+                    break;
+
+                case NEED_WRAP:
+                    return OpResult.SCHEDULE_WRAP;
+
+                case FINISHED:
+                case NOT_HANDSHAKING:
+                    return OpResult.STATE_CHANGE;
+            }
+        } while (true);
+    }
+
+    @Override
+    public OpResult stepWrap(NetReader src, NetBuffer dest) {
+        do {
+            SSLEngineResult result = src.wrap(mSSLEngine, dest);
+            switch (result.getStatus()) {
+                case BUFFER_OVERFLOW:
+                    int newBufSize = mSSLEngine.getSession().getPacketBufferSize();
+                    if (!resizeOverflowedBuffer(dest, newBufSize)) {
+                        // dest was not empty.
+                        return OpResult.DRAIN_DEST_BUFFER;
+                    }
+
+                    // Otherwise, dest was resized.  Continue looping to retry.
+                    break;
+
+                case BUFFER_UNDERFLOW:
+                    // This shouldn't happen when handshaking unless we've dropped out of the
+                    // handshake state.  Let the subsequent handshake switch() handle things.
+                    break;
+
+                case CLOSED:
+                    return OpResult.ENGINE_CLOSE;
+
+                case OK:
+                    // We might have changed out of the handshaking state so just note the read.
+                    // This is vacuous (though it consumes some ops) if we are still handshaking.
+                    src.updateRead();
+
+                    // Be greedy, and continue wrapping.
+                    break;
+            }
+
+            switch (result.getHandshakeStatus()) {
+                case NEED_TASK:
+                    return OpResult.SCHEDULE_TASKS;
+
+                case NEED_UNWRAP:
+                    return OpResult.SCHEDULE_UNWRAP;
+
+                case NEED_WRAP:
+                    break;
+
+                case FINISHED:
+                case NOT_HANDSHAKING:
+                    return OpResult.STATE_CHANGE;
+            }
+        } while (true);
+    }
+}
