97b59bfee5e853e8d6a9e579df64aaf9bcd56b0a
Kevin Ko <kevin.s.ko@gmail.com>
2014-06-02 16:39:39 -0500

    Add singleton methods for the factories.

--- src/main/com/faveset/khttp/HeapByteBufferFactory.java
+++ src/main/com/faveset/khttp/HeapByteBufferFactory.java
@@ -5,6 +5,18 @@ package com.faveset.khttp;
 import java.nio.ByteBuffer;
 
 class HeapByteBufferFactory implements ByteBufferFactory {
+    private static HeapByteBufferFactory sFactory;
+
+    /**
+     * Single accessor for a shared ByteBufferFactory.  The returned factory is NOT thread-safe.
+     */
+    public static synchronized ByteBufferFactory get() {
+        if (sFactory == null) {
+            sFactory = new HeapByteBufferFactory();
+        }
+        return sFactory;
+    }
+
     @Override
     public ByteBuffer make(int size) {
         return ByteBuffer.allocate(size);
