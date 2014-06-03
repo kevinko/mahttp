4b406f9003ce5d2166945739b7521228a656c191
Kevin Ko <kevin.s.ko@gmail.com>
2014-06-03 11:00:06 -0500

    Add keystore, truststore, and keypass flags for enabling SSL support.

--- src/main/com/faveset/khttpserver/HttpServer.java
+++ src/main/com/faveset/khttpserver/HttpServer.java
@@ -3,6 +3,11 @@
 package com.faveset.khttpserver;
 
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
 import sun.misc.Signal;
 import sun.misc.SignalHandler;
 
@@ -12,13 +17,23 @@ import com.faveset.khttp.HttpResponseWriter;
 
 import com.faveset.flags.BoolFlag;
 import com.faveset.flags.Flags;
+import com.faveset.flags.StringFlag;
 import com.faveset.log.OutputStreamLog;
 
 public class HttpServer {
-    private static BoolFlag mLogFlag =
+    private static BoolFlag sLogFlag =
         Flags.registerBool("log", false, "enable logging to stdout");
 
-    public static void main(String[] args) throws IOException, IllegalArgumentException {
+    private static StringFlag sKeyStoreFlag = Flags.registerString("keystore", "", "keystore file");
+
+    private static StringFlag sTrustStoreFlag =
+        Flags.registerString("truststore", "", "truststore file");
+
+    private static StringFlag sKeyPassFlag =
+        Flags.registerString("keypass", "", "password for private key/cert");
+
+    public static void main(String[] args) throws CertificateException, IOException,
+           IllegalArgumentException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
         Flags.parse(args);
 
         Flags flags = Flags.get();
@@ -36,11 +51,19 @@ public class HttpServer {
         final com.faveset.khttp.HttpServer server = new com.faveset.khttp.HttpServer();
 
         OutputStreamLog log = null;
-        if (mLogFlag.get()) {
+        if (sLogFlag.get()) {
             System.out.println("logging to stdout");
             log = new OutputStreamLog(System.out);
             server.setLog(log);
         }
+
+        if (!sKeyStoreFlag.get().isEmpty() &&
+                !sTrustStoreFlag.get().isEmpty()) {
+            System.out.println("using keystore (" + sKeyStoreFlag.get() + ") truststore (" +
+                    sTrustStoreFlag.get() + ")");
+            server.configureSSL(sKeyStoreFlag.get(), sTrustStoreFlag.get(), sKeyPassFlag.get());
+        }
+
 
         // Set up the signal handler for exiting the server.
         Signal.handle(new Signal("INT"), new SignalHandler() {
