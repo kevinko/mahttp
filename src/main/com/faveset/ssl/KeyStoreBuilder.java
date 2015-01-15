914a14a827ea36e695439c28e8bae13505f92759
Kevin Ko <kevin.s.ko@gmail.com>
2015-01-15 17:32:22 -0600

    Revise header copyright to refer to LICENSE.

--- src/main/com/faveset/ssl/KeyStoreBuilder.java
+++ src/main/com/faveset/ssl/KeyStoreBuilder.java
@@ -1,4 +1,6 @@
-// Copyright 2014, Kevin Ko <kevin@faveset.com>
+// Copyright 2014, Kevin Ko <kevin@faveset.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package com.faveset.ssl;
 
